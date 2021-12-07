caa9ae58cde171c7d41339ee7facc5516f972a49
Bducrocq <bducrocq42@gmail.com>
2021-12-07 13:21:56 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,38 @@
 /*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 20:00:04 by bducrocq          #+#    #+#             */
-/*   Updated: 2021/12/07 12:36:26 by bducrocq         ###   ########.fr       */
+/*   Updated: 2021/12/07 13:21:04 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-//int	 
+char	*memory_process(char *str)
+{
+	if (!str)
+		return(NULL);
+	if (ft_strchr(str, '\n'))
+		printf ("memory_process =|| %s ||\n", str);
+	return (str);
+}
  
 char	*get_next_line(int fd)
 {	
 	char			buf[BUFFER_SIZE + 1];
 	size_t			i;
 	size_t			ret;
-	static char		*memory= NULL;
+	static char		*memo = NULL;
 	char			*tmp;
 
 	ret = 1;
 	tmp = ft_strdup("");
 	if (read(fd, buf, 0) < 0)
 		return (NULL);
+	if (memo != NULL && (!ft_strchr(memo, '\n')))   // TODO:  FIXME: reprendre ICI poru gerer 
+	{
+		memo = memory_process(memo);
+		return	
+	}
 	while (!ft_strchr(tmp, '\n') && ret != 0)// boucle tant que pas de \n ou ret 0
 	{
 		ret = read(fd, buf, BUFFER_SIZE);	// protege un cas error si read -1
@@ -39,7 +51,9 @@ char	*get_next_line(int fd)
 	}
 	if (ft_strchr(tmp, '\n'))
 	{
-		memory = malloc
+		memo = malloc(sizeof(char) * ((ft_strlen(tmp) + 1)));
+		memo = ft_strchr(tmp, '\n');
+		printf("memo = %s\n",  memo);
 	}
 	return(tmp);
 }
