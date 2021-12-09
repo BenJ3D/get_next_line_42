66cad484c6cae247e5d48443ecf5d5b8363b5e7d
Bducrocq <bducrocq42@gmail.com>
2021-12-09 18:34:48 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 20:00:04 by bducrocq          #+#    #+#             */
-/*   Updated: 2021/12/09 18:01:36 by bducrocq         ###   ########.fr       */
+/*   Updated: 2021/12/09 18:34:46 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_strichr(const char *s, int c)   // analyse buff qui peut commencer par de
 
 	while (i <= BUFFER_SIZE)
 	{
-		if (s[i] != '\n')
+		if (s[i] == '\n')
 			return (i);
 		i++;
 	}
@@ -42,39 +42,39 @@ static size_t	ft_strlen_gnl(const char *str)
 	return (i);
 }
 
-char	*ft_strjoin_gnl(char *s1_line, char *s2_buf, int buf_end, int start_join)
-{
-	size_t	i;
-	size_t	j;
-	int		len_s1;
-	int		len_s2;
-	char	*str;
+// char	*ft_strjoin_gnl(char *s1_line, char *s2_buf, int buf_end, int start_join)
+// {
+// 	size_t	i;
+// 	size_t	j;
+// 	int		len_s1;
+// 	int		len_s2;
+// 	char	*str;
 
-	len_s1 = ft_strlen(s1_line);
-	len_s2 = ft_strlen(s2_buf);
-	// TODO: gerer le joint si s1_line ou s2_buf sont vide -->> strdup ?
-	if (!s1_line)
-		s1_line = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!s2_buf)
-		s2_buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	str = ft_calloc(len_s1 + len_s2 + 1, sizeof(char));
-	if (!str)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1_line[i])
-		str[j++] = s1_line[i++];
-	i = 0;
-	while (start_join != 0)
-	{
-		str[j++] = s2_buf[i++];
-		start_join--;
-	}
-	str[j] = '\0';
-	free(s1_line);
-	//free(s2_buf);
-	return (str);
-}
+// 	len_s1 = ft_strlen(s1_line);
+// 	len_s2 = ft_strlen(s2_buf);
+// 	// TODO: gerer le joint si s1_line ou s2_buf sont vide -->> strdup ?
+// 	if (!s1_line)
+// 		s1_line = malloc(sizeof(char) * BUFFER_SIZE + 1);
+// 	if (!s2_buf)
+// 		s2_buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+// 	str = ft_calloc(len_s1 + len_s2 + 1, sizeof(char));
+// 	if (!str)
+// 		return (NULL);
+// 	i = 0;
+// 	j = 0;
+// 	while (s1_line[i])
+// 		str[j++] = s1_line[i++];
+// 	i = 0;
+// 	while (start_join != 0)
+// 	{
+// 		str[j++] = s2_buf[i++];
+// 		start_join--;
+// 	}
+// 	str[j] = '\0';
+// 	free(s1_line);
+// 	//free(s2_buf);
+// 	return (str);
+// }
 
 char	*get_next_line(int fd)
 {	
@@ -86,17 +86,19 @@ char	*get_next_line(int fd)
 
 ret = 1;
 line = ft_strdup("");
-while (!(ft_strichr(line, '\n') == -1) && ret != 0)	// boucle tant que pas de \n ou ret 0 
+while ((eol = ft_strichr(buf, '\n') == -1) && ret != 0)	// boucle tant que pas de \n ou ret 0 
 	{
-		eol = ft_strichr(buf, '\n');
+		//eol = ft_strichr(buf, '\n');
 		if (eol == -1)
 			eol = BUFFER_SIZE; 
 		ret = read(fd, buf, BUFFER_SIZE);	// protege un cas error si read -1
 		if (ret <= 0)
 			return (0);
 		buf[ret] = '\0';
-		line = ft_strjoin_gnl(line, buf, BUFFER_SIZE, ft_strichr(buf, '\n')); // modifier le joint pour prendre en compte lindex	
+		line = ft_strjoin_gnl(line, buf, BUFFER_SIZE); // modifier le joint pour prendre en compte lindex	
 	}
+	eol = ft_strichr(buf, '\n');
+	
 	//ft_strichr lire le buf, index juskau \n return position du n ou -1 
 		// if (ft_strichr >= 0)
 		// 	ft_strlcpy(line, buf, ft_strichr(buf));
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 18:33:21 by bducrocq          #+#    #+#             */
-/*   Updated: 2021/12/09 17:17:25 by bducrocq         ###   ########.fr       */
+/*   Updated: 2021/12/09 18:03:25 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,28 @@ char	*get_line(char *str)  //copy juska trouver \n ou \0 et return le result
 		}
 	}
 	return (tmp);
+}
+
+char	*ft_strjoin_gnl(char *s1, char *s2, int buf_end)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	//s2[buf_end] = '\0';
+	if (!s1)
+		return (0);
+	str = ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+	str[j] = '\0';
+	free(s1);
+	return (str);
 }
